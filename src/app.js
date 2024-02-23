1b73e2797b3ef54a9abe38266abf7d8c4071ffeb
KimJaeYeon~ <popcon940620@gmail.com>
2024-02-23 20:34:24 +0900

    add 기본세팅

---
+++ src/app.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import { redisClient } from "./model/redis.js"
+
+import "dotenv/config";
+
+
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use(redisClient)
+
+//이 위로 app.use()를 넣어주세요
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  const statusCode = err.statusCode ?? 500;
+  const message = err.message ?? "서버 에러 발생";
+  const boolean = err.boolean ?? false;
+  res.status(statusCode).json({ success: boolean, message: message });
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`${process.env.PORT}번 포트로 서버 실행중`);
+});
---
+++ src/model/redis.js
@@ -0,0 +1,15 @@
+import redis from "redis";
+import "dotenv/config";
+
+export const redisClient = redis.createClient({
+  url: `${process.env.REDIS}`,
+  legacyMode: true, // 반드시 설정 !!
+});
+redisClient.on("connect", () => {
+  console.info("Redis connected!");
+});
+redisClient.on("error", (err) => {
+  console.error("Redis Client Error", err);
+});
+redisClient.connect().then();
+export const redisCli = redisClient.v4;
