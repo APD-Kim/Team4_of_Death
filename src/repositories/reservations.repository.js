02b94cbe890d7ac2f42f34344083d2ea90cf5b75
wogjs5656 <wogjs5656@gmail.com>
2024-02-24 15:20:03 +0900

    [feat] reservationRepo

--- src/repositories/reservations.repository.js
+++ src/repositories/reservations.repository.js
@@ -2,4 +2,29 @@ export class ReservationRepository {
   constructor(prisma) {
     this.prisma = prisma;
   }
+
+  // 트래이너의 가능한 날짜 찾기
+  findPossibleDates = async (trainerId) => {
+    const today = new Date();
+    const PossibleDates = await this.prisma.reservations.findMany({
+      where: {
+        trainerId: +trainerId,
+        startDate: {
+          gte: today.toISOString(),
+        },
+      },
+      select: {
+        reservationId: true,
+        startDate: true,
+        endDate: true,
+        trainers: {
+          petCategory: true,
+        },
+      },
+      orderBy: {
+        startDate: 'asc',
+      },
+    });
+    return PossibleDates;
+  };
 }
