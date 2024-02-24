990b74d3a36e78fc3203d45f2d0c998abd496a8e
KimJaeYeon~ <popcon940620@gmail.com>
2024-02-24 16:22:23 +0900

    add 토큰검증 미들웨어

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,6 +1,5 @@
 import CustomError from "../utils/errorHandler.js";
-import pkg from "jsonwebtoken";
-const { sign, verfiy } = pkg;
+import { signToken } from "../utils/auth.js";
 export class UserController {
 
   constructor(userService) {
@@ -19,10 +18,6 @@ export class UserController {
       if (password !== passwordCheck) {
         throw new CustomError(400, "비밀번호를 다시 확인하세요.")
       }
-      const findUser = await this.userService.findUserByEmail(email)
-      if (findUser) {
-        throw new CustomError(400, "이미 해당 이메일이 존재합니다.")
-      }
       const singedUser = await this.userService.signUp(email, password, name, phoneNumber, petCategory)
 
       res.status(201).json({ message: "회원가입 완료", data: singedUser })
@@ -34,23 +29,19 @@ export class UserController {
     try {
       const { email, password } = req.body;
       if (!email || !password) {
-        throw new CustomError(400, "요청이 잘못 되었습니다.")
+        throw new CustomError(400, "요청이 잘못 되었습니다.");
       }
-      const user = await this.userService.validUser(email, password)
+      const user = await this.userService.validUser(email, password);
+      const { userId } = user;
       //이메일과 비밀번호가 일치하는지 확인함
-      const token = sign(
-        { userId: user.userId },
-        process.env.SECRET_KEY,
-        { expiresIn: '1h' })
-      res.cookie('authorization', token, {
-        httpOnly: true,
-        secure: true,
-        sameSite: 'strict',
-        expires: new Date(Date.now() + 1000 * 60 * 60)
-      })
-      res.status(200).json({ accessToken: token })
+      const tokens = await signToken(res, userId);
+      res.status(200).json({ accessToken: tokens.token, refreshToken: tokens.refreshToken });
     } catch (err) {
-      next(err)
+      next(err);
     }
+  }
+  issueRefreshToken = async (req, res, next) => {
+    const { userId } = req.user;
+    res.status(200).json({ message: "good" })
   }
 }
