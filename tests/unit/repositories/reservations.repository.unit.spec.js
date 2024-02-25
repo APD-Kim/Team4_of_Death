bd66d1071e91fc2f811131ac3f0c3ed8ba71a04e
wogjs5656 <wogjs5656@gmail.com>
2024-02-26 00:04:33 +0900

    [Test] findReservationById 테스트코드 추가

--- tests/unit/repositories/reservations.repository.unit.spec.js
+++ tests/unit/repositories/reservations.repository.unit.spec.js
@@ -1,9 +1,13 @@
-import { jest } from '@jest/globals';
+import { expect, jest } from '@jest/globals';
 import { ReservationRepository } from '../../../src/repositories/reservations.repository.js';
 
 let mockPrisma = {
   reservations: {
-    findMany: jest.fn()
+    findMany: jest.fn(),
+    findFirst: jest.fn(),
+    findUnique: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn(),
   },
 };
 
@@ -14,13 +18,29 @@ describe('Reservation Repository Unit Test', () => {
     jest.resetAllMocks();
   });
 
-  test('findPossibleDates Method ', async () => {
-    const mockReturn = 'create Return string';
+  test('findPossibleDates Method Success', async () => {
+    const mockReturn = 'findPossibleDates Return string';
     mockPrisma.reservations.findMany.mockReturnValue(mockReturn);
 
     const reservations = await reservationRepository.findPossibleDates(1);
-    
+
     expect(mockPrisma.reservations.findMany).toHaveBeenCalledTimes(1);
     expect(reservations).toBe(mockReturn);
+  });
+
+  test('findReservationById Method Success', async () => {
+    const mockReturn = 'reservationId Return string';
+    const findReservationId = {
+      reservationId: 1,
+    };
+    mockPrisma.reservations.findFirst.mockReturnValue(mockReturn);
+    const findData = await reservationRepository.findReservationById(findReservationId.reservationId);
+    expect(findData).toEqual(mockReturn);
+    expect(mockPrisma.reservations.findFirst).toHaveBeenCalledTimes(1);
+    expect(mockPrisma.reservations.findFirst).toHaveBeenCalledWith({
+      where: {
+        reservationId: findReservationId.reservationId,
+      },
+    });
   });
 });
