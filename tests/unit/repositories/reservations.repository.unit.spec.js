bf74aef53f1de201ee0e1bb3e57b96f47caeae90
wogjs5656 <wogjs5656@gmail.com>
2024-02-26 00:31:52 +0900

    [Test] update delete 테스트 패스

--- tests/unit/repositories/reservations.repository.unit.spec.js
+++ tests/unit/repositories/reservations.repository.unit.spec.js
@@ -19,6 +19,7 @@ describe('Reservation Repository Unit Test', () => {
   });
 
   test('findPossibleDates Method Success', async () => {
+    // mock된 Prisma함수가 반환할 가짜 정보를 정의
     const mockReturn = 'findPossibleDates Return string';
     mockPrisma.reservations.findMany.mockReturnValue(mockReturn);
 
@@ -45,7 +46,7 @@ describe('Reservation Repository Unit Test', () => {
   });
 
   test('fincReservationByIdUnique Method Success', async () => {
-    const mockReturn = 'reservationId Return string'; 
+    const mockReturn = 'reservationId Return string';
     const findReservationId = {
       reservationId: 1,
     };
@@ -55,9 +56,43 @@ describe('Reservation Repository Unit Test', () => {
     expect(mockPrisma.reservations.findUnique).toHaveBeenCalledTimes(1);
     expect(mockPrisma.reservations.findUnique).toHaveBeenCalledWith({
       where: {
-        reservationId:findReservationId.reservationId,
+        reservationId: findReservationId.reservationId,
+      },
+    });
+  });
+
+  test('updateReservaion Method Success', async () => {
+    const mockReturn = 'updateReservation data Return string';
+    const reservationId = 1;
+    const startDate = '2024-02-26T16:49:51.038Z';
+    const endDate = '2024-02-27T16:49:51.038Z';
+
+    mockPrisma.reservations.update.mockReturnValue(mockReturn);
+    const updatedReservation = await reservationRepository.updateReservation(reservationId, startDate, endDate);
+    expect(updatedReservation).toEqual(mockReturn);
+    expect(mockPrisma.reservations.update).toHaveBeenCalledTimes(1);
+    expect(mockPrisma.reservations.update).toHaveBeenCalledWith({
+      where: {
+        reservationId: +reservationId,
+      },
+      data: {
+        startDate,
+        endDate,
+      },
+    });
+  });
+
+  test('deleteReservation Method Success', async () => {
+    const mockReturn = 'reservationdId Return string';
+    const reservationId = 1;
+    mockPrisma.reservations.delete.mockReturnValue(mockReturn);
+    const deleteReservation = await reservationRepository.deleteReservation(reservationId);
+    expect(deleteReservation).toEqual(mockReturn);
+    expect(mockPrisma.reservations.delete).toHaveBeenCalledTimes(1);
+    expect(mockPrisma.reservations.delete).toHaveBeenCalledWith({
+      where: {
+        reservationId: +reservationId,
       }
-    })
-  }) 
-  
+    });
+  });
 });
