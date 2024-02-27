38b4fe3e6f7fca9404ef1315c4cf8bd8cef7ff21
ooommma <paosige@gmail.com>
2024-02-28 02:43:58 +0900

    테스트 코드 작성

--- src/services/trainers.service.js
+++ src/services/trainers.service.js
@@ -5,95 +5,49 @@ export class TrainerService {
     this.trainerRepository = trainerRepository;
   }
 
-  registerTrainer = async (
-    userId,
-    price,
-    career,
-    petCategory,
-    address
-  ) => {
-    const user =
-      await this.trainerRepository.findOneUser(
-        userId
-      );
-
+  registerTrainer = async (userId, price, career, petCategory, address) => {
+    const user = await this.trainerRepository.findOneUser(userId);
+    console.log(user);
     if (user !== null) {
       if (user.users.isTrainer === true) {
-        throw new CustomError(
-          400,
-          '이미 트레이너로 등록된 사용자 입니다.'
-        );
+        throw new CustomError(400, '이미 트레이너로 등록된 사용자 입니다.');
       }
     }
 
     if (!career || !petCategory || !address) {
-      throw new CustomError(
-        400,
-        '트레이너 등록을 위한 필수 정보가 누락되었습니다.'
-      );
+      throw new CustomError(400, '트레이너 등록을 위한 필수 정보가 누락되었습니다.');
     }
-    const trainers =
-      await this.trainerRepository.registerTrainer(
-        userId,
-        price,
-        career,
-        petCategory,
-        address
-      );
+    const trainers = await this.trainerRepository.registerTrainer(userId, price, career, petCategory, address);
     return trainers;
   };
 
   findAllTrainer = async () => {
-    const trainer =
-      await this.trainerRepository.findAllTrainer();
+    const trainer = await this.trainerRepository.findAllTrainer();
     if (!trainer.length) {
-      throw new CustomError(
-        400,
-        '트레이너가 없습니다.'
-      );
+      throw new CustomError(400, '트레이너가 없습니다.');
     }
     return trainer;
   };
 
   findOneTrainer = async (trainerId) => {
-    const trainer =
-      await this.trainerRepository.findOneTrainer(
-        trainerId
-      );
+    const trainer = await this.trainerRepository.findOneTrainer(trainerId);
     if (!trainer) {
-      throw new CustomError(
-        404,
-        '트레이너가 존재하지 않습니다.'
-      );
+      throw new CustomError(404, '트레이너가 존재하지 않습니다.');
     }
     return trainer;
   };
 
   LikeTrainer = async (userId, trainerId) => {
-    const findLike =
-      await this.trainerRepository.existLike(
-        userId,
-        trainerId
-      );
+    const findLike = await this.trainerRepository.existLike(userId, trainerId);
     if (findLike) {
-      await this.trainerRepository.cancelLikeTrainer(
-        userId,
-        trainerId
-      );
+      await this.trainerRepository.cancelLikeTrainer(userId, trainerId);
       return {
         status: 'cancelLiked',
       };
     }
-    const liked =
-      await this.trainerRepository.LikeTrainer(
-        userId,
-        trainerId
-      );
+    const liked = await this.trainerRepository.LikeTrainer(userId, trainerId);
     if (!liked) {
-      throw new CustomError(
-        400,
-        '좋아요 생성중 오류가 발생했습니다.'
-      );
+      throw new CustomError(400, '좋아요 생성중 오류가 발생했습니다.');
     }
     return {
       status: 'liked',
@@ -102,42 +56,20 @@ export class TrainerService {
 
   /**카테고리별 펫시터 조회 */
   findTrainerByCategory = async (category) => {
-    const trainerList =
-      await this.trainerRepository.findTrainerByCategory(
-        category
-      );
+    const trainerList = await this.trainerRepository.findTrainerByCategory(category);
     if (!trainerList) {
-      throw new CustomError(
-        400,
-        '해당 카테고리내 트레이너가 존재하지 않습니다.'
-      );
+      throw new CustomError(400, '해당 카테고리내 트레이너가 존재하지 않습니다.');
     }
     return trainerList;
   };
 
-  updateTrainer = async (
-    trainerId,
-    career,
-    petCategory,
-    address,
-    price
-  ) => {
-    const trainer =
-      await this.trainerRepository.updateTrainer(
-        trainerId,
-        career,
-        petCategory,
-        address,
-        price
-      );
+  updateTrainer = async (trainerId, career, petCategory, address, price) => {
+    const trainer = await this.trainerRepository.updateTrainer(trainerId, career, petCategory, address, price);
     return trainer;
   };
 
   deleteTrainer = async (trainerId) => {
-    const trainer =
-      await this.trainerRepository.deleteTrainer(
-        trainerId
-      );
+    const trainer = await this.trainerRepository.deleteTrainer(trainerId);
     return trainer;
   };
 }
--- tests/unit/repositories/trainers.repository.unit.spec.js
+++ tests/unit/repositories/trainers.repository.unit.spec.js
@@ -81,14 +81,22 @@ describe('Trainer Repository Unit Test', () => {
     expect(trainerRepository.prisma.trainers.findMany).toHaveBeenCalledTimes(1);
   });
 
-  it('findOneUser', async () => {
+  it('findOneTrainer', async () => {
     const mockReturn = 'findFirst String';
-    mockPrisma.trainers.findFirst.mockReturnValue(mockReturn);
+    mockPrisma.trainers.findFirst.mockResolvedValue(mockReturn);
 
-    const findOneTrainerData = await trainerRepository.findOneTrainer();
+    const findOneTrainerData = await trainerRepository.findOneTrainer(1);
 
     expect(findOneTrainerData).toEqual(mockReturn);
     expect(trainerRepository.prisma.trainers.findFirst).toHaveBeenCalledTimes(1);
+    // expect(trainerRepository.prisma.trainers.findFirst).toHaveBeenCalledWith({
+    //   where: {
+    //     reviewId: +1,
+    //   },
+    //   include: {
+    //     userId: true,
+    //   },
+    // });
   });
 
   it('updateTrainer', async () => {
@@ -123,5 +131,34 @@ describe('Trainer Repository Unit Test', () => {
     expect(mockPrisma.trainers.delete).toHaveBeenCalledWith({
       where: { trainerId: +1 },
     });
+  });
+
+  it('findOneUser', async () => {
+    const mockReturn = 'findFirst String';
+    mockPrisma.trainers.findFirst.mockReturnValue(mockReturn);
+
+    const findOneUserData = await trainerRepository.findOneUser(1);
+
+    expect(findOneUserData).toEqual(mockReturn);
+    expect(trainerRepository.prisma.trainers.findFirst).toHaveBeenCalledTimes(1);
+    // expect(trainerRepository.prisma.trainers.findFirst).toHaveBeenCalledWith(
+    //   {
+    //     userId: 1,
+    //   },
+    //   {
+    //     users: {
+    //       name: '김라임',
+    //       email: 'popcon9406201@gmail.com',
+    //       password: '123456',
+    //       profileImg: null,
+    //       phoneNumber: '010-4311-1111',
+    //       role: 'user',
+    //       petCategory: 'cat',
+    //       createdAt: '2024-02-24T05:09:47.025Z',
+    //       updatedAt: '2024-02-24T05:09:47.025Z',
+    //       isTrainer: false,
+    //     },
+    //   }
+    // );
   });
 });
